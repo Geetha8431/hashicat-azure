bd31d07f190da00cb2871f050c1489c7dcc9d0db
Geetha V <geethav843182@gmail.com>
2024-10-07 08:54:09 +0000

    Added use for each in network

--- network.tf
+++ network.tf
@@ -2,5 +2,6 @@ module "network" {
   source  = "app.terraform.io/AzureOrg1/network/azurerm"
   version = "3.5.0"
   # insert required variables here
+  use_for_each = true 
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
